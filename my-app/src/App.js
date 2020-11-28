c6c786e60cd984efa16bde52db0c65c57a90d74b
audreywrong <68037586+audreywrong@users.noreply.github.com>
2020-11-28 14:59:43 -0500

    game link working
    set up react router
    established game page
    home link works
    game link works

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,55 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+//import { Homepage } from './features/homepage/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="router-wrapper">
+      <BrowserRouter>
+        <switch>
+          <Route exact path='/' component={Counter}/>
+          <Route exact path='/game' component={Counter}/>
+        </switch>
+            <nav>
+        <Link className="nav-item" to='/'>Home</Link>
+        <Link className="nav-item" to='/game'>Game</Link>
+      </nav>
+      </BrowserRouter>
     </div>
   );
 }
