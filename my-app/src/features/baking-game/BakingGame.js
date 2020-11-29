de40a5e2d48477218a351573671d61b2f1063424
audreywrong <68037586+audreywrong@users.noreply.github.com>
2020-11-29 14:01:10 -0500

    proceed button functionality
    useState
    useRef
    useEffect
    anonymous click handler expanded into click handler function
    handleClick function used by onClick
    improved variable naming
    better readability and intuition

--- my-app/src/features/baking-game/BakingGame.js
+++ my-app/src/features/baking-game/BakingGame.js
@@ -1,16 +1,31 @@
-import {React, useState} from 'react';
+import {React, useEffect, useRef, useState} from 'react';
 import './BakingGame.css';
 import breadLoadingScreen from '../../bread-loading-screen.png';
 
 function BakingGame() {
-  const [nextButtonText, setNextButtonText] = useState(`Let's Bake`);  
+
+  const [currentStepNumber, setCurrentStepNumber] = useState(0);
+  const proceedButtonRef = useRef(`Let's Bake!`); 
+
+  useEffect(() => {
+    proceedButtonRef.current = steps[currentStepNumber + 1].buttonText;
+  }, [currentStepNumber]);
+
+  function handleClick() {
+    setCurrentStepNumber(currentStepNumber + 1);
+    console.log(proceedButtonRef)
+  }
+
   return (
       <>
       <img src={breadLoadingScreen} className="App-loading-pic" alt="loading" />
-      <button onClick={() => setNextButtonText(`Knead on to step 2!`)}>{nextButtonText}</button>
+      <button onClick = {handleClick}>
+      {proceedButtonRef.current}</button>
       </>
     )
   }
 
+  // type = step[]
+  const steps = [{stepNumber: 0, buttonText: `Let's Bake!`}, {stepNumber: 1, buttonText: `Time to mix!`}, {stepNumber: 2, buttonText: `It's aliiiiive!`}, {stepNumber: 3, buttonText: `Ah, shape it, shape it good, sh-sh-shape it real good!`}, {stepNumber: 4, buttonText: `It has to rise. Again.`}, {stepNumber: 5, buttonText: `Time to take a little nap!`}, {stepNumber: 6, buttonText: `Bake this!`}, {stepNumber: 7, buttonText: `Let's get this bread!`}, {stepNumber: 8}]
 
 export default BakingGame;
