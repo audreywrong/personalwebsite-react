e6fc1c51ffa254b5e1cf10f7bf864eb75f1d672f
audreywrong <68037586+audreywrong@users.noreply.github.com>
2020-11-30 18:59:37 -0500

    img ref onclick
    imported images
    added images to steps array
    updated variable cohesiveness and readability

--- my-app/src/features/baking-game/BakingGame.js
+++ my-app/src/features/baking-game/BakingGame.js
@@ -1,31 +1,34 @@
 import {React, useEffect, useRef, useState} from 'react';
 import './BakingGame.css';
 import breadLoadingScreen from '../../bread-loading-screen.png';
+import recipe from '../../images/recipe.png';
+import frankenBread from '../../images/frankenbread.jpg';
+import mixingBowl from '../../images/mixing-bowl.png';
+import breadShaping from '../../images/bread-shaping.jpg';
 
 function BakingGame() {
 
   const [currentStepNumber, setCurrentStepNumber] = useState(0);
-  const proceedButtonRef = useRef(`Let's Bake!`); 
+  const currentStepRef = useRef(steps[0]); 
 
   useEffect(() => {
-    proceedButtonRef.current = steps[currentStepNumber + 1].buttonText;
+    currentStepRef.current = steps[currentStepNumber + 1];
   }, [currentStepNumber]);
 
   function handleClick() {
     setCurrentStepNumber(currentStepNumber + 1);
-    console.log(proceedButtonRef)
   }
 
   return (
       <>
-      <img src={breadLoadingScreen} className="App-loading-pic" alt="loading" />
+      <img src={currentStepRef.current.image} className="App-loading-pic" alt="loading" />
       <button onClick = {handleClick}>
-      {proceedButtonRef.current}</button>
+      {currentStepRef.current.buttonText}</button>
       </>
     )
   }
 
   // type = step[]
-  const steps = [{stepNumber: 0, buttonText: `Let's Bake!`}, {stepNumber: 1, buttonText: `Time to mix!`}, {stepNumber: 2, buttonText: `It's aliiiiive!`}, {stepNumber: 3, buttonText: `Ah, shape it, shape it good, sh-sh-shape it real good!`}, {stepNumber: 4, buttonText: `It has to rise. Again.`}, {stepNumber: 5, buttonText: `Time to take a little nap!`}, {stepNumber: 6, buttonText: `Bake this!`}, {stepNumber: 7, buttonText: `Let's get this bread!`}, {stepNumber: 8}]
+  const steps = [{stepNumber: 0, buttonText: `Let's Bake!`, image: breadLoadingScreen}, {stepNumber: 1, buttonText: `Time to mix!`, image: recipe}, {stepNumber: 2, buttonText: `It's aliiiiive!`, image: mixingBowl}, {stepNumber: 3, buttonText: `Ah, shape it, shape it good, sh-sh-shape it real good!`, image: frankenBread}, {stepNumber: 4, buttonText: `It has to rise. Again.`, image: breadShaping}, {stepNumber: 5, buttonText: `Time to take a little nap!`}, {stepNumber: 6, buttonText: `Bake this!`}, {stepNumber: 7, buttonText: `Let's get this bread!`}, {stepNumber: 8}]
 
 export default BakingGame;
