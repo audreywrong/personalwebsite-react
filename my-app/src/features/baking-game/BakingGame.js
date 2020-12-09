b787e8882f6c3f0f377a84bac3b82c156a044263
audreywrong <68037586+audreywrong@users.noreply.github.com>
2020-12-09 17:35:04 -0500

    bakery name added
    form submission for bakery name
    input bakery name at top of page for duration of game

--- my-app/src/features/baking-game/BakingGame.js
+++ my-app/src/features/baking-game/BakingGame.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useRef, useState} from 'react';
+import {React, useEffect, useRef, useState, Component} from 'react';
 import './BakingGame.css';
 import breadLoadingScreen from '../../images/bread-loading-screen.png';
 import recipe from '../../images/recipe.png';
@@ -13,6 +13,7 @@ import burntBread from '../../images/burnt-bread.png';
 
 function BakingGame() {
   const [currentStepNumber, setCurrentStepNumber] = useState(0);
+  const [bakeryName, setBakeryName] = useState('');
   const currentStepRef = useRef(steps[0]);
 
   useEffect(() => {
@@ -23,13 +24,24 @@ function BakingGame() {
     }
   }, [currentStepNumber]);
 
+  function handleBakeryNameForm(newBakeryName) {
+    setBakeryName(newBakeryName);
+  }
+
   function handleClick() {
     setCurrentStepNumber(currentStepNumber + 1);
   }
 
   return (
     <>
-      <div className="image-wrapper"><img src={currentStepRef.current.image} className={currentStepRef.current.imgClass} alt="logo" /></div>
+      {(() => {
+        debugger;
+        return bakeryName && <h1>{bakeryName}</h1>;
+      })()}
+      <div className="image-wrapper">
+        <img src={currentStepRef.current.image} className={currentStepRef.current.imgClass} alt="logo" />
+      </div>
+      <NameForm handleBakeryNameForm={handleBakeryNameForm}></NameForm>
       <button onClick={handleClick}>{currentStepRef.current.buttonText}</button>
     </>
   );
@@ -52,5 +64,41 @@ const steps = [
   {stepNumber: 7, buttonText: `Let's get this bread!`, image: baking, imgClass: 'still-image'},
   {stepNumber: 8, buttonText: `You win! Want to bake again?`, image: freshLoaf, imgClass: 'floating-image'},
 ];
+
+//Step 1 mini game
+
+class NameForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    debugger;
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    debugger;
+    event.preventDefault();
+    this.props.handleBakeryNameForm(this.state.value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter bakery name here:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 export default BakingGame;
