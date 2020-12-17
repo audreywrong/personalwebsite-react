bb3fcc470102478137af20b8c82f2a81ff2f1e91
Vandivier <john@afterecon.com>
2020-12-16 23:08:48 -0500

    feat: initial working puzzle

--- my-app/src/features/baking-game/photo-puzzle/Puzzle.jsx
+++ my-app/src/features/baking-game/photo-puzzle/Puzzle.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-import Board from "./Board";
+import React from 'react';
+import Board from './Board';
 import frankenbread from '../../../images/frankenbread.png';
 import './Puzzle.css';
 
 function Puzzle() {
+  return (
+    <div className="puzzle">
+      <Board imgUrl={frankenbread} />
+    </div>
+  );
+}
 
-    return (
-      <div className="puzzle">
-        <h1>PLEASE WORK DEAR GOD.</h1>
-        <Board imgUrl={frankenbread} />
-      </div>
-    );
-  }
-  
-  export default Puzzle;
-  
+export default Puzzle;
