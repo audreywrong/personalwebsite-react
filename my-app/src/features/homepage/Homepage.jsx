bdad16633995a2166efbbdd5e2c02e8dfd1d21e8
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-01-19 18:09:42 -0500

    homepage styling

--- my-app/src/features/homepage/Homepage.jsx
+++ my-app/src/features/homepage/Homepage.jsx
@@ -1,99 +1,102 @@
 import React from 'react';
 import './Homepage.css';
+import headshot from '../../images/headshot.jpg';
 
 function Homepage() {
   return (
-    <>
-      <p>body</p>
-      <p>h1</p>
-      <h1>Hi, I'm Audrey, web developer.</h1>
-      <p id="h1-tag">/h1</p>
-      <p>/body</p>
-      <p>/html</p>
-    </>
-  );
-}
-
-{
-  /* <span class="blast" aria-hidden="true" style="opacity: 1;">
-          H
-        </span>
-        <span class="blast" aria-hidden="true" style="opacity: 1;">
-          i
-        </span>
-        <span class="blast" aria-hidden="true" style="opacity: 1;">
-          ,
-        </span>
-        <br>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            I
+    <div class="row">
+      <div class="column">
+        <p>{'<body>'}</p>
+        <p>{'<h1>'}</p>
+        <h1>
+          <span class="blast" aria-hidden="true">
+            H
           </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            '
+          <span class="blast" aria-hidden="true">
+            i
           </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            m
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            A
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            u
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            d
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            r
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            e
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            y
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
+          <span class="blast" aria-hidden="true">
             ,
           </span>
-        </br>
-        <br>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            w
+          <br />
+          <span class="blast" aria-hidden="true">
+            I
           </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            e
+          <span class="blast" aria-hidden="true">
+            '
           </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            b
+          <span class="blast" aria-hidden="true">
+            m&nbsp;
           </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
+          <span class="blast" aria-hidden="true">
+            A
+          </span>
+          <span class="blast" aria-hidden="true">
+            u
+          </span>
+          <span class="blast" aria-hidden="true">
             d
           </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            e
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            v
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            e
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            l
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            o
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            p
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
-            e
-          </span>
-          <span class="blast" aria-hidden="true" style="opacity: 1;">
+          <span class="blast" aria-hidden="true">
             r
           </span>
-        </br> */
+          <span class="blast" aria-hidden="true">
+            e
+          </span>
+          <span class="blast" aria-hidden="true">
+            y
+          </span>
+          <span class="blast" aria-hidden="true">
+            ,
+          </span>
+
+          <br />
+          <span class="blast" aria-hidden="true">
+            w
+          </span>
+          <span class="blast" aria-hidden="true">
+            e
+          </span>
+          <span class="blast" aria-hidden="true">
+            b&nbsp;
+          </span>
+          <span class="blast" aria-hidden="true">
+            d
+          </span>
+          <span class="blast" aria-hidden="true">
+            e
+          </span>
+          <span class="blast" aria-hidden="true">
+            v
+          </span>
+          <span class="blast" aria-hidden="true">
+            e
+          </span>
+          <span class="blast" aria-hidden="true">
+            l
+          </span>
+          <span class="blast" aria-hidden="true">
+            o
+          </span>
+          <span class="blast" aria-hidden="true">
+            p
+          </span>
+          <span class="blast" aria-hidden="true">
+            e
+          </span>
+          <span class="blast" aria-hidden="true">
+            r
+          </span>
+        </h1>
+        <p>{'</h1>'}</p>
+        <p>{'</body>'}</p>
+        <p>{'</html>'}</p>
+      </div>
+      <div class="column">
+        <img src={headshot} alt="Headshot" className="headshot" />
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
