6a02840191ccb057d5afd0138430a91218849b5f
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-02-06 11:31:11 -0500

    fixed minor errors

--- my-app/src/features/homepage/Homepage.jsx
+++ my-app/src/features/homepage/Homepage.jsx
@@ -4,96 +4,96 @@ import headshot from '../../images/headshot.jpg';
 
 function Homepage() {
   return (
-    <div class="row">
-      <div class="column left-column">
-        <p class="tag">{'<html>'}</p>
-        <p class="tag margin-left-65">{'<body>'}</p>
-        <p class="tag margin-left-125">{'<h1>'}</p>
+    <div className="row">
+      <div className="column left-column">
+        <p className="tag">{'<html>'}</p>
+        <p className="tag margin-left-65">{'<body>'}</p>
+        <p className="tag margin-left-125">{'<h1>'}</p>
         <h1>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             H
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             i
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             ,
           </span>
           <br />
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             I
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             '
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             m&nbsp;
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             A
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             u
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             d
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             r
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             e
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             y
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             ,
           </span>
 
           <br />
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             w
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             e
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             b&nbsp;
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             d
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             e
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             v
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             e
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             l
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             o
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             p
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             e
           </span>
-          <span class="blast" aria-hidden="true">
+          <span className="blast" aria-hidden="true">
             r
           </span>
         </h1>
-        <p class="tag margin-left-125">{'</h1>'}</p>
-        <p class="tag margin-left-65">{'</body>'}</p>
-        <p class="tag">{'</html>'}</p>
+        <p className="tag margin-left-125">{'</h1>'}</p>
+        <p className="tag margin-left-65">{'</body>'}</p>
+        <p className="tag">{'</html>'}</p>
       </div>
-      <div class="column right-column">
+      <div className="column right-column">
         <img src={headshot} alt="Headshot" className="headshot" />
       </div>
     </div>
