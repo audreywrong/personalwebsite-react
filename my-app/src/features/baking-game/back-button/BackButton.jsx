b1a9e0e5f447392bd20ea3f3e96d168a73c178b6
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-02-06 11:31:43 -0500

    button styles

--- my-app/src/features/baking-game/back-button/BackButton.jsx
+++ my-app/src/features/baking-game/back-button/BackButton.jsx
@@ -4,7 +4,7 @@ const BackButton = props =>
   props.bakeryName &&
   props.currentStep.stepNumber >= 1 &&
   props.currentStep.stepNumber < 8 && (
-    <Button onClick={props.handleClickDecrement} variant="contained">
+    <Button onClick={props.handleClickDecrement} variant="contained" style={{display: 'block', margin: '.5rem auto'}}>
       IF I COULD TURN BACK TIME...
     </Button>
   );
