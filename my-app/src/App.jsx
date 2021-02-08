50436734d94b8448cbc38d4de769297369c9a346
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-02-08 16:12:30 -0500

    social follow icons
    imported react icons
    linked icons to social urls

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
-//import { Counter } from './features/counter/Counter';
 import './App.css';
 import {BrowserRouter, Route, Link, Redirect} from 'react-router-dom';
 import {Switch} from 'react-router';
 import Homepage from './features/homepage/Homepage';
 import BakingGame from './features/baking-game/BakingGame';
+import {FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa';
 
 function App() {
   return (
     <div className="router-wrapper">
       <BrowserRouter>
-        <nav>
-          <Link className="nav-item" to="/personalwebsite-react">
-            Home
-          </Link>
-          {/* <Link className="nav-item">Resume</Link> */}
-          <Link className="nav-item" to="/baking-game">
-            Baking Game
-          </Link>
-          {/* <Link className="nav-item">Contact Me</Link> */}
-        </nav>
+        <div id="nav-bar">
+          <nav>
+            <Link className="nav-item" to="/personalwebsite-react">
+              Home
+            </Link>
+            {/* <Link className="nav-item">Resume</Link> */}
+            <Link className="nav-item" to="/baking-game">
+              Baking Game
+            </Link>
+            <Link className="social-item" to="https://github.com/audreywrong">
+              <FaGithub />
+            </Link>
+            <Link className="social-item" to="https://twitter.com/audwrong">
+              <FaTwitter />
+            </Link>
+            <Link className="social-item" to="https://www.linkedin.com/in/audreylwright/">
+              <FaLinkedin />
+            </Link>
+          </nav>
+        </div>
         <Switch>
           <Route exact path="/personalwebsite-react" component={Homepage} />
           <Route exact path="/baking-game" component={BakingGame} />
