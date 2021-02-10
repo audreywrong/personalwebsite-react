ff9e5c0f4b4e717e55e8b9bc64d7120c67084ac0
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-02-09 19:55:12 -0500

    AboutMe changes
    style adjustements
    added content

--- my-app/src/features/about-me/AboutMe.jsx
+++ my-app/src/features/about-me/AboutMe.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './AboutMe.css';
+import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+
+import './AboutMe.css';
 import Bread from './bread.jpg';
 import Concert from './concert.JPG';
-import Hiking from './hiking.JPG';
 import GamerGirl from './skulllogo.png';
+import Hiking from './hiking.JPG';
 import HobbyItem from './HobbyItem';
 
 const AboutMe = () => (
@@ -13,59 +15,92 @@ const AboutMe = () => (
     <Grid container spacing={1} justify="center">
       <Grid item xs={10} sm={6}>
         <Paper>
-          <h1>Professional Summary</h1>
-          <p>
-            Software Engineer with a proven ability to adapt to a changing, collaborative environment and deliver
-            detail-oriented results.
+          <Box m={2} p={2}>
+            <h1>Professional Summary</h1>
             <p>
-              Before transitioning to coding, I came from a fast-paced entrepreneurial and sales-focused career where I
-              always found ways to integrate efficient, high-performing technology into my day-to-day work environment.
-              This included CRM's like Insightly, social media marketing, ad campaigns and e-mail campaigns.{' '}
+              Software Engineer with a proven ability to adapt to a changing, collaborative environment and deliver
+              detail-oriented results.
+              <p>
+                Before transitioning to coding, I came from a fast-paced entrepreneurial and sales-focused career where
+                I always found ways to integrate efficient, high-performing technology into my day-to-day work
+                environment. This included CRM's like Insightly, social media marketing, ad campaigns and e-mail
+                campaigns.{' '}
+              </p>
+              <p>
+                Discovering that I possess a passion for the technology itself (that extended beyond my love of video
+                games and computers since before I could fully speak) has pushed me to pursue software engineering and
+                develop my skills as a Front End Developer with further aspirations that I continue to work towards.
+              </p>
+              <p>
+                I believe that you are never an "expert" and that there is ALWAYS something more to learn! Collaborate,
+                ask questions and never fear trying. I'm excited and passionate about building fully responsive web
+                applications that help solve complex problems!
+              </p>
             </p>
-            <p>
-              Discovering that I possess a passion for the technology itself (that extended beyond my love of video
-              games and computers since before I could fully speak) has pushed me to pursue software engineering and
-              develop my skills as a Front End Developer with further aspirations that I continue to work towards.
-            </p>
-            <p>
-              I believe that you are never an "expert" and that there is ALWAYS something more to learn! Collaborate,
-              ask questions and never fear trying. I'm excited and passionate about building fully responsive web
-              applications that help solve complex problems!
-            </p>
-          </p>
+          </Box>
         </Paper>
       </Grid>
       <Grid item xs={6} sm={3}>
         <Paper>
-          <h1>Technical Skills</h1>
-          <ul>
-            <li>Languages: HTML5, CSS3, JavaScript (ES6), EcmaScript</li>
-            <li>Frameworks: Node.js, Create React APP, Gatsby</li>
-            <li>Libraries: React, Redux, Material-UI</li>
-            <li>CI/CD: GIT, GitHub Pages</li>
-            <li>Tools: GitHub, VS Code</li>
-          </ul>
+          <Box m={2} p={2}>
+            <h1>Technical Skills</h1>
+            <ul>
+              <li>Languages: HTML5, CSS3, JavaScript (ES6), EcmaScript</li>
+              <li>Frameworks: Node.js, Create React APP, Gatsby</li>
+              <li>Libraries: React, Redux, Material-UI</li>
+              <li>CI/CD: GIT, GitHub Pages</li>
+              <li>Tools: GitHub, VS Code</li>
+            </ul>{' '}
+          </Box>
         </Paper>
       </Grid>
       <Grid item xs={6} sm={3}>
         <Paper>
-          <h1>Additional</h1>
-          <ul>
-            <li>Google Analytics</li>
-            <li>Web and Social Media Analytics</li>
-            <li>Ad Spend</li>
-            <li>Strategic Digital Messaging</li>
-            <li>Insightly</li>
-            <li>Monday.com</li>
-          </ul>
+          <Box m={2} p={2}>
+            <h1>Additional</h1>
+            <ul>
+              <li>Google Analytics</li>
+              <li>Web and Social Media Analytics</li>
+              <li>Ad Spend</li>
+              <li>Strategic Digital Messaging</li>
+              <li>Insightly</li>
+              <li>Monday.com</li>
+            </ul>
+          </Box>
         </Paper>
       </Grid>
-      <HobbyItem {...{imgSource: Bread, imgAltText: 'Bread', hobbyText: 'This is some info about my hobby.'}} />
-      <HobbyItem {...{imgSource: Hiking, imgAltText: 'Hiking', hobbyText: 'This is some info about my hobby.'}} />
       <HobbyItem
-        {...{imgSource: GamerGirl, imgAltText: 'Gamer Girl Logo', hobbyText: 'This is some info about my hobby.'}}
+        {...{
+          imgSource: Bread,
+          imgAltText: 'Bread',
+          hobbyText:
+            "I have loved baking since I can remember, and I still have the 'Betty Crocker's Boys and Girls Cook Book' that was given to me when I was 10. My most recent baking interest has been with sourdough bread! I also enjoy making cookies and other breads. In 2020 I had the opportunity to participate in the Bakers Against Racism nationwise bake sale to raise money for a local organization helping in the fight against racism. Baking is a mix of science and art and I love being able to gift what I make to others. #bodybybread",
+        }}
       />
-      <HobbyItem {...{imgSource: Concert, imgAltText: 'Concert', hobbyText: 'This is some info about my hobby.'}} />
+      <HobbyItem
+        {...{
+          imgSource: Hiking,
+          imgAltText: 'Hiking',
+          hobbyText:
+            "Hiking has allowed me to see some of the most incredible sights. The physical and mental work you put in to reach the reward of the view at the end is a feeling I can't get over. From craters to forest trails I've been fortunate enough to see some incredible things. One of my most favorite hikes is Glymur Waterfall in Iceland (pictured here). It was a spontaneous hike that was hard to find, but was an experience I don't know if I can ever replicate.",
+        }}
+      />
+      <HobbyItem
+        {...{
+          imgSource: GamerGirl,
+          imgAltText: 'Gamer Girl Logo',
+          hobbyText:
+            "GamerGirl is a brand and business concept I developed in a High School Capstone course. It is 'Gamer Girl is an innovative, inclusive, creative and edgy gaming accessory company for the female gaming community of today, or Gamer Girls!' I sketched designs for prototypes, learned how to write a formal business plan and even made some of the prototypes! Who knows?! Maybe it'll make a comeback in the future!",
+        }}
+      />
+      <HobbyItem
+        {...{
+          imgSource: Concert,
+          imgAltText: 'Concert',
+          hobbyText:
+            'From singing The Beatles with my dad in the living room to going to as many concerts as I can with my sister; music is as much a part of me as the hair on my head. I will listen to just about anything, but truly love rock and metal, classical and dance music. Going to live shows with friends and family is one of my favorite things in the world!',
+        }}
+      />
     </Grid>
   </div>
 );
--- my-app/src/features/about-me/HobbyItem.jsx
+++ my-app/src/features/about-me/HobbyItem.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import './AboutMe.css';
+import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+
+import './AboutMe.css';
 
 const HobbyItem = props => (
   <Grid item xs={12} sm={6} className="hobby">
     <Paper>
-      <Grid container>
-        <Grid item xs={3}>
-          <img src={props.imgSource} alt={props.imgAltText} className="hobby-image" />
+      <Box m={2} p={2}>
+        <Grid container>
+          <Grid item xs={3}>
+            <img src={props.imgSource} alt={props.imgAltText} className="hobby-image" />
+          </Grid>
+          <Grid item xs={9}>
+            <p style={{marginLeft: '1.5rem'}}>{props.hobbyText}</p>
+          </Grid>
         </Grid>
-        <Grid item xs={9}>
-          <p>{props.hobbyText}</p>
-        </Grid>
-      </Grid>
+      </Box>
     </Paper>
   </Grid>
 );
