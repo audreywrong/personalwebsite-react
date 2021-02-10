c84fab5c07dd0279508a18e2898b82c02f928d87
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-02-10 16:53:05 -0500

    linked mobile nav

--- my-app/src/features/mobile-menu/MobileMenu.jsx
+++ my-app/src/features/mobile-menu/MobileMenu.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
+import {React, useState} from 'react';
+import {Link} from 'react-router-dom';
 import {GiHamburger} from 'react-icons/gi';
 import {makeStyles} from '@material-ui/core/styles';
+import {FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 const useStyles = makeStyles({
   list: {
     width: 250,
   },
-  fullList: {
-    width: 'auto',
-  },
 });
 
 export default function MobileMenu() {
   const classes = useStyles();
-  // theme.breakpoints.up('sm')
-  const [state, setState] = React.useState({
+  const [state, setState] = useState({
     isOpen: false,
   });
 
@@ -32,21 +30,49 @@ export default function MobileMenu() {
     setState({...state, isOpen: open});
   };
 
-  const list = anchor => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
+  const navArray = [
+    {linkContent: 'Home', hasLink: true, linkHref: '/personalwebsite-react'},
+    {linkContent: 'About Me', hasLink: true, linkHref: '/about-me'},
+    {
+      linkContent: 'Blog',
+      hasLink: false,
+
+      linkHref: 'https://audreywrong.github.io/all-about-audrey/',
+    },
+    {linkContent: 'Baking Game', hasLink: true, linkHref: '/baking-game'},
+    {linkContent: <FaGithub />, hasLink: false, linkHref: 'https://github.com/audreywrong'},
+    {linkContent: <FaTwitter />, hasLink: false, linkHref: 'https://twitter.com/audwrong'},
+    {
+      linkContent: <FaLinkedin />,
+      hasLink: false,
+
+      linkHref: 'https://www.linkedin.com/in/audreylwright/',
+    },
+  ];
+
+  const MobileNavList = () => (
+    <div style={{width: 'auto'}} role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        {navArray.map(({linkContent, isLink, linkHref}) => {
+          const hasSocial = typeof linkContent !== 'string';
+          const listContent = hasSocial ? (
+            <ListItemIcon>{linkContent}</ListItemIcon>
+          ) : (
+            <ListItemText>{linkContent}</ListItemText>
+          );
+
+          return (
+            <ListItemText button key={linkContent}>
+              {isLink ? (
+                <Link to={linkHref}>{listContent}</Link>
+              ) : (
+                <a href={linkHref} target="_blank">
+                  {listContent}
+                </a>
+              )}
+            </ListItemText>
+          );
+        })}
       </List>
     </div>
   );
@@ -60,19 +86,9 @@ export default function MobileMenu() {
       >
         <GiHamburger style={{height: '1.5rem', width: '1.5rem'}} />
       </Button>
-      )
-      {/* {['Home', 'About Me', 'Baking Game', 'bottom'].map(anchor => (
-        <React.Fragment key={anchor}>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))} */}
+      <SwipeableDrawer open={state.isOpen} onClose={toggleDrawer(false)}>
+        <MobileNavList />
+      </SwipeableDrawer>
     </div>
   );
 }
