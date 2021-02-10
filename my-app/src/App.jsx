f95f4705700825aaccda498ecf4467d0f2290c27
audreywrong <68037586+audreywrong@users.noreply.github.com>
2021-02-09 21:20:08 -0500

    mobile hamburger menu

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,41 +1,49 @@
 import React from 'react';
-import './App.css';
+import {FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa';
 import {BrowserRouter, Route, Link, Redirect} from 'react-router-dom';
 import {Switch} from 'react-router';
+
+import './App.css';
 import Homepage from './features/homepage/Homepage';
 import BakingGame from './features/baking-game/BakingGame';
 import AboutMe from './features/about-me/AboutMe';
-import {FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa';
+import MobileMenu from './features/mobile-menu/MobileMenu';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 function App() {
+  const isDesktop = useMediaQuery('(min-width:600px)');
   return (
     <div className="router-wrapper">
       <BrowserRouter>
         <div id="nav-bar">
-          <nav>
-            <Link className="nav-item" to="/personalwebsite-react">
-              Home
-            </Link>
-            <Link className="nav-item" to="/about-me">
-              About Me
-            </Link>
-            {/* <Link className="nav-item">Resume</Link> */}
-            <Link className="nav-item" to="/baking-game">
-              Baking Game
-            </Link>
-            {/* <a className="nav-item" href="https://audreywrong.github.io/all-about-audrey/" target="_blank">
+          {isDesktop ? (
+            <nav>
+              <Link className="nav-item" to="/personalwebsite-react">
+                Home
+              </Link>
+              <Link className="nav-item" to="/about-me">
+                About Me
+              </Link>
+              {/* <Link className="nav-item">Resume</Link> */}
+              <Link className="nav-item" to="/baking-game">
+                Baking Game
+              </Link>
+              {/* <a className="nav-item" href="https://audreywrong.github.io/all-about-audrey/" target="_blank">
               Blog
             </a> */}
-            <a className="social-item" href="https://github.com/audreywrong" target="_blank">
-              <FaGithub />
-            </a>
-            <a className="social-item" href="https://twitter.com/audwrong" target="_blank">
-              <FaTwitter />
-            </a>
-            <a className="social-item" href="https://www.linkedin.com/in/audreylwright/" target="_blank">
-              <FaLinkedin />
-            </a>
-          </nav>
+              <a className="social-item" href="https://github.com/audreywrong" target="_blank">
+                <FaGithub />
+              </a>
+              <a className="social-item" href="https://twitter.com/audwrong" target="_blank">
+                <FaTwitter />
+              </a>
+              <a className="social-item" href="https://www.linkedin.com/in/audreylwright/" target="_blank">
+                <FaLinkedin />
+              </a>
+            </nav>
+          ) : (
+            <MobileMenu />
+          )}
         </div>
         <Switch>
           <Route exact path="/personalwebsite-react" component={Homepage} />
---
+++ my-app/src/features/mobile-menu/MobileMenu.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import clsx from 'clsx';
+import {GiHamburger} from 'react-icons/gi';
+import {makeStyles} from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function MobileMenu() {
+  const classes = useStyles();
+  // theme.breakpoints.up('sm')
+  const [state, setState] = React.useState({
+    isOpen: false,
+  });
+
+  const toggleDrawer = open => event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({...state, isOpen: open});
+  };
+
+  const list = anchor => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      <Button
+        onClick={toggleDrawer(true)}
+        style={{backgroundColor: 'white', border: '2px solid black', padding: '2px 8px'}}
+        className="mobile-menu"
+      >
+        <GiHamburger />
+      </Button>
+      )
+      {/* {['Home', 'About Me', 'Baking Game', 'bottom'].map(anchor => (
+        <React.Fragment key={anchor}>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))} */}
+    </div>
+  );
+}
